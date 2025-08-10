1a05033ca934e6b9e65d9d242f923cc7e6b227b4
Smart <knisansala14@gmail.com>
2025-08-10 14:02:17 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const[count,setCount]=useState(0);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCount(count+1);
+      
+    },1000)
+  })
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />use effect
+        <h1>
+
+        </h1>
+        <p>
+          {count}
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
